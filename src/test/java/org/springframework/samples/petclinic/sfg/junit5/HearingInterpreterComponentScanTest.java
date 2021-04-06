90ccbff41502bd4b9a83992ce02dd0a7b5a6e245
Developer <jjstrain68@gmail.com>
2021-04-06 12:02:54 +0100

    Setup profiles and active profiles for running tests

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
@@ -2,28 +2,24 @@ package org.springframework.samples.petclinic.sfg.junit5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
-import org.springframework.samples.petclinic.sfg.LaurelWordProducer;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ActiveProfiles("inner-class")
-@SpringJUnitConfig(classes = HearingInterpreterInnerClassTest.TestConfig.class)
-class HearingInterpreterInnerClassTest {
+@ActiveProfiles("component-scan")
+@SpringJUnitConfig(classes = HearingInterpreterComponentScanTest.TestConfig.class)
+class HearingInterpreterComponentScanTest {
 
-    @Profile("inner-class")
+    @Profile("component-scan")
     @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
     static class TestConfig {
 
-        @Bean
-        HearingInterpreter hearingInterpreter(){
-            return new HearingInterpreter(new LaurelWordProducer());
-        }
     }
 
     @Autowired
